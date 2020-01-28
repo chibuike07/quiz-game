e1e6123ffaea1e2a3cd1e088071221c786432f6f
hobbyluv <princewillchime43@gmail.com>
2020-01-28 17:15:54 +0100

    the arr and obj file

---
+++ quiz -and-ans.js
@@ -0,0 +1,163 @@
+let arrObj = [
+  {
+    quis: "what year did nigeria got independent",
+    ans: "1960"
+  },
+  {
+    quis: "who wrote the novel called 'things fall apart'",
+    ans: "'chinuachebe'"
+  },
+  {
+    quis: "which country won in the 1996 world cup",
+    ans: "'france'"
+  },
+  {
+    quis: "who was the winner of 2019 big brother naija show",
+    ans: "'mercy'"
+  },
+  {
+    quis: "which country is the most beautifull county in the whole world",
+    ans: "'paris, france'"
+  },
+  {
+    quis: "Which substance is used to retard the setting action of cement?",
+    ans: "CaSO.2HO"
+  },
+  {
+    quis: "who was the author of the Arthashastra",
+    ans: "Chanakya"
+  },
+  {
+    quis:
+      "Which country’s football team has won the 2018 Intercontinental Cup football title",
+    ans: "India"
+  },
+  {
+    quis: "Who has won the men’s singles French Open tennis tournament 2018",
+    ans: "Rafael Nadal"
+  },
+  {
+    quis: "The 2019 Eurovision Song Contest was held in which city",
+    ans: "Tel Aviv"
+  },
+  {
+    quis:
+      "Following a long running dispute with Greece, the Republic of Macedonia renamed itself as what",
+    ans: "The Republic of North Macedonia"
+  },
+  {
+    quis:
+      "The plane carrying which footballer to Cardiff City crashed in the English Channel on 21 January",
+    ans: "Emiliano Sala"
+  },
+  {
+    quis: "In April a major fire broke out in which iconic building in France",
+    ans: "Notre Dame"
+  },
+  {
+    quis:
+      "Joaquin Guzman was sentenced to life in prison plus 30 years for drug trafficking, but what is he better known as",
+    ans: "El Chapo"
+  },
+  {
+    quis: "The 2019 Cricket World Cup was won by which country",
+    ans: "England"
+  },
+  {
+    quis: "The FIFA Women’s World Cup was held in which country in 2019",
+    ans: "France"
+  },
+  {
+    quis:
+      "In March according to Forbes who became the world’s youngest billionaire at just 21",
+    ans: "Kylie Jenner"
+  },
+  {
+    quis:
+      "Which Travel Company, one of the largest and oldest in the world went bankrupt in 2019",
+    ans: "Thomas Cook"
+  },
+  {
+    quis: "Which Jamaican rapper released 'Boombastic' in 1995",
+    ans: "Shaggy"
+  },
+  {
+    quis:
+      "Complete the title of this 1995 U2 hit 'Hold Me Thrill Me Kiss Me ...'",
+    ans: "Kill Me"
+  },
+  {
+    quis:
+      "Rod Stewart and Sting teamed up with which North American singer for the 'Three Musketeers' single 'All for Love' in 1993",
+    ans: "Bryan Adams"
+  },
+  {
+    quis: "“A Diamond is Forever”",
+    ans: "De Beers"
+  },
+  {
+    quis: "“Because You’re Worth It”",
+    ans: "L’Oreal"
+  },
+  {
+    quis: "“Challenge Everything”",
+    ans: "EA Games"
+  },
+  {
+    quis: "“The Make Up of Make Up Artists”",
+    ans: "Max Factor"
+  },
+  {
+    quis: "“The best a man can get”",
+    ans: "Gillette"
+  },
+  {
+    quis: "“Just Do It”",
+    ans: "Nike"
+  },
+  {
+    quis: "“The World’s Local Bank”",
+    ans: "HSBC"
+  },
+  {
+    quis: "“It Does Exactly What it Says on the Tin”",
+    ans: "Ronseal"
+  },
+  {
+    quis: "“Vorsprung durch Technik”",
+    ans: "Audi"
+  },
+  {
+    quis: "“It could be you!”",
+    ans: "The National Lottery"
+  }
+];
+
+let price = [
+  "$20",
+  "$50",
+  "$100",
+  "$200",
+  "$300",
+  "$400",
+  "$500",
+  "$600",
+  "$700",
+  "$800",
+  "$900",
+  "$1000"
+];
+let priceB = [
+  "$1500",
+  "$2000",
+  "$2500",
+  "$3000",
+  "$3500",
+  "$4000",
+  "$4500",
+  "$5000",
+  "$5500",
+  "$6000",
+  "$6500",
+  "$7000"
+];
